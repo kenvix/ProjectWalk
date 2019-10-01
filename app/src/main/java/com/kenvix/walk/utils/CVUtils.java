25df35d401434180c2456a5eb1156c8d3ff6abc8
Kenvix <kenvixzure@live.com>
2019-10-01 11:55:17 +0800

    OpenCV Configured

---
+++ app/src/main/java/com/kenvix/walk/utils/CVUtils.java
@@ -0,0 +1,5 @@
+package com.kenvix.walk.utils;
+
+public final class CVUtils {
+
+}
--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -3,8 +3,4 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE    := main-lib
 LOCAL_SRC_FILES := jni-main.c
-LOCAL_EXPORT_LDLIBS := -llog
-LOCAL_LDLIBS := -llog
-LOCAL_LDFLAGS := -llog
-LOCAL_SHARED_LIBRARIES += liblog
 include $(BUILD_SHARED_LIBRARY)
