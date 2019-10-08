64f4b0231e90402ff8491fe19b654e07755a7acb
Kenvix <kenvixzure@live.com>
2019-10-08 20:10:01 +0800

    Finish recognizer

--- app/src/main/java/com/kenvix/walk/ui/recognizer/RecognizerActivity.kt
+++ app/src/main/java/com/kenvix/walk/ui/recognizer/RecognizerActivity.kt
@@ -9,6 +9,7 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.ScrollView
 import android.support.v7.widget.Toolbar
+import android.view.View
 import android.widget.TextView
 import com.kenvix.utils.android.annotation.ViewAutoLoad
 import com.kenvix.walk.ApplicationEnvironment
@@ -21,14 +22,24 @@ class RecognizerActivity : BaseActivity() {
     @ViewAutoLoad lateinit var recognizerImage: ImageView
     @ViewAutoLoad lateinit var recognizerScroll: ScrollView
     @ViewAutoLoad lateinit var recognizerToolbar: Toolbar
+    @ViewAutoLoad lateinit var recognizerTitle: TextView
     @ViewAutoLoad lateinit var recognizerTextCalorie: TextView
     @ViewAutoLoad lateinit var recognizerTextName: TextView
     @ViewAutoLoad lateinit var recognizerTextProbability: TextView
+    @ViewAutoLoad lateinit var recognizerTextProblem0: TextView
+    @ViewAutoLoad lateinit var recognizerTextProblem1: TextView
+    @ViewAutoLoad lateinit var recognizerTextProblem2: TextView
+    @ViewAutoLoad lateinit var recognizerTextProblem3: TextView
 
+    private lateinit var loadingDialog: android.app.AlertDialog
     private lateinit var fileUri: Uri
 
     override fun onInitialize(savedInstanceState: Bundle?) {
         val contentUri = intent.getStringExtra("image_content_uri")
+        recognizerTitle.text = getString(R.string.loading)
+        loadingDialog = showProgressDialog("正在识别，请稍候 ...", true, null) {
+            if(!it) finish()
+        }
 
         if (contentUri != null) {
             fileUri = Uri.parse(contentUri)
@@ -59,6 +70,8 @@ class RecognizerActivity : BaseActivity() {
     }
 
     fun onRecognizerCompleted(result: RecognizerRequestResult?) {
+        loadingDialog.dismiss()
+
         if (result != null) {
             if (result.status != 0)
                 return onRecognizerFailed(result.info)
@@ -68,15 +81,30 @@ class RecognizerActivity : BaseActivity() {
             if (foods.size <= 0)
                 return onRecognizerFailed("抱歉，暂时不支持识别此菜品")
 
-            recognizerTextName.text = foods[0].name
-            recognizerTextCalorie.text = foods[0].calorie
-            recognizerTextProbability.text = foods[0].probability
+            recognizerScroll.visibility = View.VISIBLE
+            recognizerTitle.text = getString(R.string.title_recognize_result)
+            val i = 0
+
+            recognizerTextName.text = foods[i].name
+            recognizerTextCalorie.text = foods[i].calorie
+            recognizerTextProbability.text = foods[i].probability
+
+            if (foods[i].isHighSaltAndOil) {
+                recognizerTextProblem0.setTextColor(getColor(R.color.warning))
+                recognizerTextProblem0.text = "盐分和油脂含量过高"
+                recognizerTextProblem1.visibility = View.VISIBLE
+                recognizerTextProblem1.setTextColor(getColor(R.color.terminalText))
+                recognizerTextProblem1.text = "这可能会增加患心血管疾病的风险，请考虑食用低盐食物"
+            }
+
         } else {
             onRecognizerFailed("接口返回结果不正确")
         }
     }
 
     fun onRecognizerFailed(message: String) {
+        loadingDialog.dismiss()
+
         showAlertDialog(message, "检测食物失败") {
             finish()
         }
